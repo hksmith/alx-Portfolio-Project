36fc474d7ba5ed73d803ad7a84d54aaa5e9ad482
Kalab Solomon <105478362+hksmith@users.noreply.github.com>
2023-11-15 15:04:27 +0300

    adding appropriate comment to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+## rendering the whole app
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
